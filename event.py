b77a18ca10899b7dc35a344aa4473fdb52efea6c
James Garnon <jg.gatc@gmail.com>
2014-06-13 17:00:28 -0400

    restructure event handler to isolate from userevent and jevent objects

--- event.py
+++ event.py
@@ -61,6 +61,7 @@ class Event(object):
             self.modKey = set([keycode.valueOf() for keycode in (Const.K_ALT, Const.K_CTRL, Const.K_SHIFT)])
             self.specialKey = set([keycode.valueOf() for keycode in (Const.K_UP, Const.K_DOWN, Const.K_LEFT, Const.K_RIGHT, Const.K_HOME, Const.K_END, Const.K_PAGEDOWN, Const.K_PAGEUP, Const.K_BACKSPACE, Const.K_DELETE, Const.K_INSERT, Const.K_RETURN, Const.K_TAB, Const.K_ESCAPE)])
 #Const.K_F1, Const.K_F2, Const.K_F3, Const.K_F4, Const.K_F5, Const.K_F6, Const.K_F7, Const.K_F8, Const.K_F9, Const.K_F10, Const.K_F11, Const.K_F12, Const.K_F13, Const.K_F14, Const.K_F15   #IE keypress keycode: id same as alpha keys
+        self.Event = UserEvent
         self._nonimplemented_methods()
 
     def _lock(self):
@@ -117,7 +118,7 @@ class Event(object):
         """
         self._lock()
         if not eventType:
-            self.queue = [ self.JEvent(event) for event in self.eventQueue[0:self.eventNum] ]
+            self.queue = [ JEvent(event) for event in self.eventQueue[0:self.eventNum] ]
             self.eventNum = 0
         else:
             if not isinstance(eventType, (tuple,list)):
@@ -129,7 +130,7 @@ class Event(object):
                 if self.eventQueue[i].type not in eventType:
                     queue.append(self.eventQueue[i])
                 else:
-                    self.queue.append( self.JEvent(self.eventQueue[i]) )
+                    self.queue.append( JEvent(self.eventQueue[i]) )
             if len(queue) != self.eventNum:
                 self.eventNum = len(queue)
                 for i in range(self.eventNum):
@@ -143,7 +144,7 @@ class Event(object):
         """
         self._lock()
         if self.eventNum:
-            evt = self.JEvent( self.eventQueue.pop(0) )
+            evt = JEvent( self.eventQueue.pop(0) )
             self.eventNum -= 1
             self.eventQueue.append(None)
         else:
@@ -158,7 +159,7 @@ class Event(object):
         while True:
             if self.eventNum:
                 self._lock()
-                evt = self.JEvent( self.eventQueue.pop(0) )
+                evt = JEvent( self.eventQueue.pop(0) )
                 self.eventNum -= 1
                 self.eventQueue.append(None)
                 self._unlock()
@@ -280,90 +281,92 @@ class Event(object):
         self.set_grab = lambda *arg: None
         self.get_grab = lambda *arg: False
 
-    class Event(object):
 
-        __slots__ = ['type', 'attr']
+class UserEvent(object):
 
-        def __init__(self, eventType, *args, **kwargs):
-            """
-            Return user event.
-            Argument includes eventType (USEREVENT+num).
-            Optional attribute argument as dictionary ({str:val}) or keyword arg(s).
-            """
-            if args:
-                attr = args[0]
+    __slots__ = ['type', 'attr']
+
+    def __init__(self, eventType, *args, **kwargs):
+        """
+        Return user event.
+        Argument includes eventType (USEREVENT+num).
+        Optional attribute argument as dictionary ({str:val}) or keyword arg(s).
+        """
+        if args:
+            attr = args[0]
+        else:
+            attr = kwargs
+        object.__setattr__(self, "type", eventType)
+        object.__setattr__(self, "attr", attr)
+
+    def __repr__(self):
+        """
+        Return string representation of Event object.
+        """
+        return "%s(%s-UserEvent %r)" % (self.__class__, self.type, self.attr)
+
+    def __setattr__(self, attr, value):
+        raise AttributeError, ("'Event' object has no attribute '%s'" % attr)
+
+
+class JEvent(object):
+
+    _mouse_pos = (0, 0)
+    _types = {'mousemove':Const.MOUSEMOTION, 'mousedown':Const.MOUSEBUTTONDOWN, 'mouseup':Const.MOUSEBUTTONUP, 'keydown':Const.KEYDOWN, 'keypress':Const.KEYDOWN, 'keyup':Const.KEYUP}
+    _charCode = {33:Const.K_EXCLAIM, 34:Const.K_QUOTEDBL, 35:Const.K_HASH, 36:Const.K_DOLLAR, 38:Const.K_AMPERSAND, 39:Const.K_QUOTE, 40:Const.K_LEFTPAREN, 41:Const.K_RIGHTPAREN, 42:Const.K_ASTERISK, 43:Const.K_PLUS, 44:Const.K_COMMA, 45:Const.K_MINUS, 46:Const.K_PERIOD, 97:Const.K_a, 98:Const.K_b, 99:Const.K_c, 100:Const.K_d, 101:Const.K_e, 102:Const.K_f, 103:Const.K_g, 104:Const.K_h, 105:Const.K_i, 106:Const.K_j, 107:Const.K_k, 108:Const.K_l, 109:Const.K_m, 110:Const.K_n, 111:Const.K_o, 112:Const.K_p, 113:Const.K_q, 114:Const.K_r, 115:Const.K_s, 116:Const.K_t, 117:Const.K_u, 118:Const.K_v, 119:Const.K_w, 120:Const.K_x, 121:Const.K_y, 122:Const.K_z}
+
+    def __init__(self, event):
+        """
+        Event object wraps JavaScript event, created when retrieving events from queue.
+        
+        Event object attributes:
+        
+        * type: MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION, KEYDOWN, KEYUP
+        * button: mouse button pressed (1/2/3)
+        * pos: mouse position (x,y)
+        * rel: mouse relative position change (x,y)
+        """
+        self.event = event      #__getattr__ not implemented in pyjs -O
+        if event.type in ('mousedown', 'mouseup'):
+            self.type = self.__class__._types[event.type]
+            self.button = event.button + 1
+            self.pos = event.pos[0]+env.frame.scrollLeft, event.pos[1]+env.frame.scrollTop
+        elif event.type == 'mousemove':
+            self.type = self.__class__._types[event.type]
+            self.button = event.button + 1
+            self.pos = event.pos[0]+env.frame.scrollLeft, event.pos[1]+env.frame.scrollTop
+            self.rel = (self.pos[0]-self.__class__._mouse_pos[0], self.pos[1]-self.__class__._mouse_pos[1])
+            self.__class__._mouse_pos = self.pos
+        elif event.type in ('keydown', 'keyup'):
+            self.type = self.__class__._types[event.type]
+            self.key = event.keyCode
+        elif event.type == 'keypress':
+            self.type = self.__class__._types[event.type]
+            if event.keyCode:
+                code = event.keyCode
             else:
-                attr = kwargs
-            object.__setattr__(self, "type", eventType)
-            object.__setattr__(self, "attr", attr)
-
-        def __repr__(self):
-            """
-            Return string representation of Event object.
-            """
-            return "%s(%s-UserEvent %r)" % (self.__class__, self.type, self.attr)
-
-        def __setattr__(self, attr, value):
-            raise AttributeError, ("'Event' object has no attribute '%s'" % attr)
-
-    class JEvent(object):
-
-        _mouse_pos = (0, 0)
-        _types = {'mousemove':Const.MOUSEMOTION, 'mousedown':Const.MOUSEBUTTONDOWN, 'mouseup':Const.MOUSEBUTTONUP, 'keydown':Const.KEYDOWN, 'keypress':Const.KEYDOWN, 'keyup':Const.KEYUP}
-        _charCode = {33:Const.K_EXCLAIM, 34:Const.K_QUOTEDBL, 35:Const.K_HASH, 36:Const.K_DOLLAR, 38:Const.K_AMPERSAND, 39:Const.K_QUOTE, 40:Const.K_LEFTPAREN, 41:Const.K_RIGHTPAREN, 42:Const.K_ASTERISK, 43:Const.K_PLUS, 44:Const.K_COMMA, 45:Const.K_MINUS, 46:Const.K_PERIOD, 97:Const.K_a, 98:Const.K_b, 99:Const.K_c, 100:Const.K_d, 101:Const.K_e, 102:Const.K_f, 103:Const.K_g, 104:Const.K_h, 105:Const.K_i, 106:Const.K_j, 107:Const.K_k, 108:Const.K_l, 109:Const.K_m, 110:Const.K_n, 111:Const.K_o, 112:Const.K_p, 113:Const.K_q, 114:Const.K_r, 115:Const.K_s, 116:Const.K_t, 117:Const.K_u, 118:Const.K_v, 119:Const.K_w, 120:Const.K_x, 121:Const.K_y, 122:Const.K_z}
-
-        def __init__(self, event):
-            """
-            Event object wraps JavaScript event, created when retrieving events from queue.
-            
-            Event object attributes:
-            
-            * type: MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION, KEYDOWN, KEYUP
-            * button: mouse button pressed (1/2/3)
-            * pos: mouse position (x,y)
-            * rel: mouse relative position change (x,y)
-            """
-            self.event = event      #__getattr__ not implemented in pyjs -O
-            if event.type in ('mousedown', 'mouseup'):
-                self.type = self.__class__._types[event.type]
-                self.button = event.button + 1
-                self.pos = event.pos[0]+env.frame.scrollLeft, event.pos[1]+env.frame.scrollTop
-            elif event.type == 'mousemove':
-                self.type = self.__class__._types[event.type]
-                self.button = event.button + 1
-                self.pos = event.pos[0]+env.frame.scrollLeft, event.pos[1]+env.frame.scrollTop
-                self.rel = (self.pos[0]-self.__class__._mouse_pos[0], self.pos[1]-self.__class__._mouse_pos[1])
-                self.__class__._mouse_pos = self.pos
-            elif event.type in ('keydown', 'keyup'):
-                self.type = self.__class__._types[event.type]
-                self.key = event.keyCode
-            elif event.type == 'keypress':
-                self.type = self.__class__._types[event.type]
-                if event.keyCode:
-                    code = event.keyCode
-                else:
-                    code = event.which
-                try:
-                    self.key = self.__class__._charCode[code]
-                except KeyError:
-                    self.key = code
-            else:
-                self.type = event.type
-                for attr in event.attr:
-                    object.__setattr__(self, attr, event.attr[attr])
-
-        def __repr__(self):
-            """
-            Return string representation of Event object.
-            """
+                code = event.which
             try:
-                return "%s(%s)" % (self.__class__, self.event.toString())
-            except AttributeError:      #User Event
-                return self.event.__repr__()
+                self.key = self.__class__._charCode[code]
+            except KeyError:
+                self.key = code
+        else:
+            self.type = event.type
+            for attr in event.attr:
+                object.__setattr__(self, attr, event.attr[attr])
 
-        def getEvent(self):
-            """
-            Return JavaScript event.
-            """
-            return self.event
+    def __repr__(self):
+        """
+        Return string representation of Event object.
+        """
+        try:
+            return "%s(%s)" % (self.__class__, self.event.toString())
+        except AttributeError:      #User Event
+            return self.event.__repr__()
+
+    def getEvent(self):
+        """
+        Return JavaScript event.
+        """
+        return self.event
 
