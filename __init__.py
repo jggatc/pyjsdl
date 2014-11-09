a3ecb5aabbb336eb5a2fadc6ffedd7383b44c75b
James Garnon <jg.gatc@gmail.com>
2014-11-09 17:54:11 -0500

    update documentation

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@
 #You should have received a copy of the GNU Lesser General Public License
 #along with this library; if not, see http://www.gnu.org/licenses/.
 #
-#Pyjsdl version 0.19
+#Pyjsdl version 0.20
 #Download Site: http://gatc.ca
 
 from display import Display
