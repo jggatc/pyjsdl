4a8bff3839c114834c9fdd9c8abf321a90ffc924
James Garnon <jg.gatc@gmail.com>
2016-04-24 00:24:53 -0400

    update documentation

--- env.py
+++ env.py
@@ -1,12 +1,10 @@
 #Pyjsdl - Copyright (C) 2013 James Garnon <http://gatc.ca/>
 #Released under the MIT License <http://opensource.org/licenses/MIT>
 
-"""
-Canvas object.
-"""
-
 
 canvas = None
+
 frame = None
+
 pyjs_mode = None
 
