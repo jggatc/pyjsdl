c0914993a1d4699e2021c83b18e3e1f47da75c28
James Garnon <jg.gatc@gmail.com>
2021-03-31 17:58:57 -0400

    revise requestanimationframe shim

--- pyjsobj.py
+++ pyjsobj.py
@@ -30,34 +30,40 @@ def requestAnimationFrameInit():
     return wnd()
 
 
-#requestAnimationFrame polyfill
-#derived from code by Erik Möller, fixes from Paul Irish and Tino Zijdel
-#https://gist.github.com/paulirish/1579671
-#MIT license
-requestAnimationFramePolyfill = JS("""
+def requestAnimationFramePolyfill():
+    JS(
+"""
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+
 (function() {
     var lastTime = 0;
     var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !$wnd['requestAnimationFramex']; ++x) {
-        $wnd['requestAnimationFrame'] = $wnd[vendors[x]+'RequestAnimationFramex'];
-        $wnd['cancelAnimationFrame'] = $wnd[vendors[x]+'CancelAnimationFrame']
-                                   || $wnd[vendors[x]+'CancelRequestAnimationFrame'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
     }
-
-    if (!$wnd['requestAnimationFramex'])
-        $wnd['requestAnimationFrame'] = function(callback, element) {
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = $wnd['setTimeout'](function() { callback(currTime + timeToCall); },
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
               timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
-
-    if (!$wnd['cancelAnimationFrame'])
-        $wnd['cancelAnimationFrame'] = function(id) {
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
             clearTimeout(id);
         };
-});
-""")
+}());
+"""
+    )
 
