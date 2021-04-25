e02e8a3bba7c9bb00ca657305eaadb4f1be1bbb8
James Garnon <jg.gatc@gmail.com>
2021-04-25 18:34:04 -0400

    add env check

--- env.py
+++ env.py
@@ -1,24 +1,44 @@
 #Pyjsdl - Copyright (C) 2013 James Garnon <https://gatc.ca/>
 #Released under the MIT License <https://opensource.org/licenses/MIT>
 
+import os, sys
+
+
+if os.name != 'pyjs':
+    print('Use Pyjs to compile script to a JS app')
+    sys.exit()
+
 
 canvas = None
 
 frame = None
 
+pyjs_mode = None
+
 event = None
 
-pyjs_mode = None
+
+def get_canvas():
+    """
+    Return Canvas object.
+    """
+    return canvas
+
+
+def get_frame():
+    """
+    Return Webpage frame.
+    """
+    return frame
+
+
+def get_pyjsmode():
+    """
+    Return Pyjs mode object.
+    """
+    return pyjs_mode
 
 
 def set_env(key, val):
-    global canvas, frame, event, pyjs_mode
-    if key == 'canvas':
-        canvas = val
-    elif key == 'frame':
-        frame = val
-    elif key == 'event':
-        event = val
-    elif key == 'pyjs_mode':
-        pyjs_mode = val
+    setattr(sys.modules[__name__], key, val)
 
