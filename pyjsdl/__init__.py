0d2b329100363d7cf7c272874501992386027c43
James Garnon <jg.gatc@gmail.com>
2025-01-18 21:35:25 -0500

    update documentation

--- pyjsdl/__init__.py
+++ pyjsdl/__init__.py
@@ -19,7 +19,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
 
-__version__ = '0.26'
+__version__ = '0.27'
 
 from pyjsdl import env
 from pyjsdl import util
